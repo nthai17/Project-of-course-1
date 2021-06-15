de98699e2c41d5bc3ac1f4e9959b1b3750905087
Nguyen duy thai <duythai120696@gmail.com>
2021-06-15 10:48:42 +0700

    creat language option and code js for search-client extend

--- access/js/app.js
+++ access/js/app.js
@@ -120,13 +120,76 @@ $('.tutorial .owl-carousel').owlCarousel({
 
 });
 
-let search = document.getElementsByClassName('header__search-client')[0];
+let search = document.getElementById('header__hasextend')
 let extend = document.getElementById("extend-client");
 search.onclick = function () {
-    extend.style.display = 'block';
+    if(extend.style.display == 'none') {
+        extend.style.display = 'block';
+    } else {
+        extend.style.display = 'none';
+    }
 }
-window.onclick = function (event) {
-    if (event.target != search) {
-        extend.style.display = "none"
+let isLanguage = document.getElementById('language');
+let isOption = document.getElementsByClassName('header__navbar-language')[0];
+isLanguage.onclick = function () {
+    if(isOption.style.display == 'none') {
+        isOption.style.display = 'flex';
+    } else {
+        isOption.style.display = 'none';
+    }
+}
+
+let plusAdult = document.getElementById('plusAdult');
+let minusAdult = document.getElementById('minusAdult');
+let plusChild = document.getElementById('plusChild');
+let minusChild = document.getElementById('minusChild');
+let plusInfant = document.getElementById('plusInfant');
+let minusInfant = document.getElementById('minusInfant');
+let inputAdult = document.getElementById('inputAdult').value;
+let inputChild = document.getElementById('inputChild').value;
+let inputInfant = document.getElementById('inputInfant').value;
+
+plusAdult.onclick = function (){
+    if(!isNaN(inputAdult)){
+        inputAdult++;
+        document.getElementById('inputAdult').value = inputAdult;
+        return false    
+    }
+}
+minusAdult.onclick = function (){
+    if(!isNaN(inputAdult) && inputAdult > 0){
+        inputAdult--;
+        document.getElementById('inputAdult').value = inputAdult;
+        return false    
+    }
+}
+
+plusInfant.onclick = function (){
+    if(!isNaN(inputInfant)){
+        inputInfant++;
+        document.getElementById('inputInfant').value = inputInfant;
+        return false    
+    }
+}
+minusInfant.onclick = function (){
+    if(!isNaN(inputInfant) && inputInfant > 0){
+        inputInfant--;
+        document.getElementById('inputInfant').value = inputInfant;
+        return false    
+    }
+}
+
+plusChild.onclick = function (){
+    if(!isNaN(inputChild)){
+        inputChild++;
+        document.getElementById('inputChild').value = inputChild;
+        return false    
+    }
+}
+minusChild.onclick = function (){
+    if(!isNaN(inputChild) && inputChild > 0){
+        inputChild--;
+        document.getElementById('inputChild').value = inputChild;
+        return false    
     }
 }
